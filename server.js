2fbcb0162f51065707b3d1223ecdd88533de99a5
Candyzorua <jiaying.ong2@gmail.com>
2023-01-19 21:21:58 -0800

    Update server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const https = require("https");
+
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("Hello world!")
+})
+
+app.listen(3000, function(){
+    console.log("Server is running on Port 3000.");
+})
